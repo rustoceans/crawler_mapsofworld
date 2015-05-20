e6dd704fb7c9ef4f2cc28b30764dafc589693f26
alexfalcucc <alex.falcucci@gmail.com>
2015-05-20 17:25:29 -0300

    script refactored

--- extract_norcal_table.py
+++ extract_norcal_table.py
@@ -2,14 +2,13 @@ import urllib2
 
 from bs4 import BeautifulSoup
 
-url = "http://www.mapsofworld.com/usa/states/california/map-of-northern-
-        california.html"
-        
-page = urllib2.urlopen(url)       
+url = ("http://www.mapsofworld.com/usa/states/california/"
+       "map-of-northern-california.html")
+
+page = urllib2.urlopen(url)
 soup = BeautifulSoup(page)
 
 tables = soup.findAll("table")
 tables[3].find_all('td')
 
-for td in tables[3].find_all('td'):
-    print td.text
+counties = [td.text for td in tables[3].find_all('td')]
